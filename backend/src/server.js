76c5cd411ef80bb30c3a6e02e724f35dbc974031
Santosh Kumar <santosh358mis@gmail.com>
2025-06-07 15:43:57 +0530

    DB step up and video stram

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import "dotenv/config";
+
+import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Importing routes
+
+app.use('/api/auth', authRoutes);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+  connectDB();
+});
