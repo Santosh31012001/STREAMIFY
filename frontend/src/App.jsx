26c041487ad5328ec67a52106f75bff7097d26eb
Santosh Kumar <santosh358mis@gmail.com>
2025-06-22 12:20:03 +0530

    update index.html title and favicon; add lucide-react dependency; implement auth user check in App component with protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes } from "react-router"
+import {Route, Routes,Navigate } from "react-router"
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
@@ -7,18 +7,32 @@ import CallPage from './pages/CallPage';
 import ChatPage from './pages/ChatPage';
 import OnboardingPage from './pages/OnboardingPage';
 
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios";
+
 
 const App = () => {
+  // tankStack query example
+  const { data:authData, isLoading, error } = useQuery({ 
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/api/me");
+      return res.data;
+    },
+    retry:false, // auth check
+  });
+  const authUser = authData?.user;   
+
   return (
     <div className=" h-screen" data-theme="valentine">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to="/login" />} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to="/login" />} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default  axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:5001",
     withCredentials: true, // This allows cookies to be sent with requests
 });
