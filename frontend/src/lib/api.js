55f6aca533346ebefd3bed4bc36b6945bc0038a4
Santosh Kumar <santosh358mis@gmail.com>
2025-06-29 15:40:45 +0530

    feat: enhance NotificationsPage with friend request handling and no notifications component

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -47,3 +47,13 @@ export async function sendFriendRequest(userId) {
   const response = await axiosInstance.post(`/users/friend-request/${userId}`);
   return response.data;
 };
+
+export async function getFriendRequests() {
+  const response = await axiosInstance.get("/users/friend-requests");
+  return response.data;
+}
+
+export async function acceptFriendRequest(userId) {
+  const response = await axiosInstance.put(`/users/friend-request/${userId}/accept`);
+  return response.data;
+}
