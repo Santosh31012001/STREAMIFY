084c8865fbb493b4f8a18adfb49770fc5458f480
Santosh Kumar <santosh358mis@gmail.com>
2025-07-05 00:41:12 +0530

    feat: configure backend and frontend for streamify integration, update environment variables, and enhance axios base URL handling for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,25 @@ import express from 'express';
 import "dotenv/config";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
+
+
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
+
+
+
 import { connectDB } from './lib/db.js';
 
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+const __dirname = path.resolve();
 
-
-app.use(cors({
+app.use(
+  cors({
   origin: "http://localhost:5173",
   credentials: true,   //allow frontend to access cookies
 }));
@@ -28,6 +32,16 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat' , chatRoutes);
+
+// Serve static files from the frontend build directory
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  // Serve the index.html file for any other routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
