23851ae950bf60d0d51c377ea7bda67fbc7f671f
Santosh Kumar <santosh358mis@gmail.com>
2025-07-06 18:19:48 +0530

    env update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_API_URL || "https://streamify-8.onrender.com/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://streamify-kjcm.vercel.app/api";
 
 export const axiosInstance = axios.create({
     // baseURL: "http://localhost:5001/api",
