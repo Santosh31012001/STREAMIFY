a89b15c9b343bed9d1b03feffc7ad3e9d4df3948
Santosh Kumar <santosh358mis@gmail.com>
2025-07-26 23:03:09 +0530

    fix: integrate navigation on successful logout in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
@@ -7,15 +7,14 @@ import useLogout from "../hooks/useLogout";
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
+  const navigate = useNavigate();
   const isChatPage = location.pathname?.startsWith("/chat");
 
-  // const queryClient = useQueryClient();
-  // const { mutate: logoutMutation } = useMutation({
-  //   mutationFn: logout,
-  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
-  // });
-
-  const { logoutMutation } = useLogout();
+  const { logoutMutation } = useLogout({
+    onSuccess: () => {
+      navigate("/login");
+    },
+  });
 
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
