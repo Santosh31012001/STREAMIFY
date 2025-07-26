4cea7385e52fcb3b3d016e28eab004d48e972206
Santosh Kumar <santosh358mis@gmail.com>
2025-07-26 15:53:39 +0530

    refactor: update CORS configuration and add commented local API URL in .env

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,11 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.use(
-  cors({
-    origin: '*', // Allow all origins
-  })
-);
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
